bdc148be3f977a905ee61b8ed2bee20153ae2bd7
Rhabad <nicolaspizarro.450@gmail.com>
2023-08-13 05:54:28 -0400

    Convirtidor

---
+++ src/convertidordeunidades/ConvertidorAPP.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package convertidordeunidades;
+
+import vistas.ConvertidorUnidades;
+
+/**
+ *
+ * @author NICOLAS
+ */
+public class ConvertidorAPP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ConvertidorUnidades con = new ConvertidorUnidades();
+        
+        con.setVisible(true);
+        con.setResizable(false);
+        con.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
